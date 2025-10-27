98f226c9276414e9a66cf3f1c943bee98e90247c
victorgustavodev <victorgustavo.dev@gmail.com>
2025-10-27 10:57:29 -0300

    feat: implementa `usecases` e `dtos` nas entidades `bookings, logs, participants, recurrences, rooms e users`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sistema de Reserva de Salas')
+    .setDescription('API de reservas, salas, usuários e logs')
+    .setVersion('1.0')
+    .addBearerAuth() // Para endpoints autenticados futuramente
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // mantém o token entre requisições
+    },
+  });
+
+  await app.listen(3000);
+  console.log(`🚀 API rodando em: http://localhost:3000`);
+  console.log(`📘 Swagger em: http://localhost:3000/docs`);
 }
 bootstrap();
